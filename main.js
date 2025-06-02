80cef76e4b730d86dc52109518bd3a5bc4950517
aliDibrahim <aliebraheem2192001@gmail.com>
2025-06-02 22:52:18 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// Mega Menu
+let otherLinks = document.getElementById("other-links");
+let megaMenu = document.getElementById("mega-menu");
+otherLinks.addEventListener("click", () => {
+  if (megaMenu.style.opacity == 1) {
+    megaMenu.style.top = "calc(100% + 60px)";
+    megaMenu.style.opacity = 0;
+    megaMenu.style.zIndex = -5;
+  } else {
+    megaMenu.style.top = "calc(100% + 1px)";
+    megaMenu.style.opacity = 1;
+    megaMenu.style.zIndex = 5;
+  }
+});
+// Scroll up
+let scrollUp = document.getElementById("scroll-top");
+window.onscroll = function () {
+  if (scrollY > 1000) {
+    scrollUp.style.right = "20px";
+  } else {
+    scrollUp.style.right = "-70px";
+  }
+};
+scrollUp.onclick = () => {
+  window.scrollTo({
+    top: 0,
+  });
+};
